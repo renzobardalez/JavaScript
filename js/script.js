fc1511077e4a8938d453e99e09351c78fa0cf94a
Renzo Bardalez <r.bardalez@outlook.com>
2024-10-05 19:16:01 -0500

    Inicialización

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let inventario = 0 /* true or false*/
+let tipoMovimiento = 0 /* si inventario  es true = Compra, venta, intercambio si es false = Egreso, ingreso o transferencia */
+let cuentaBancaria = 0
+let total = 0 /* total del movimiento */
+
+
