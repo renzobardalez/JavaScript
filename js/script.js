e3e136827ab45e68e9872745d5449498718bb906
Renzo Bardalez <r.bardalez@outlook.com>
2024-10-07 01:13:25 -0500

    Entregable 1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let inventario = 0 /* true or false*/
+let inventario = '' /* true or false*/
 let tipoMovimiento = 0 /* si inventario  es true = Compra, venta, intercambio si es false = Egreso, ingreso o transferencia */
 // let cuentaBancaria = 0
 //let total = 0 /* total del movimiento */
@@ -6,20 +6,45 @@ let user = ""
 
 
 do{
-    inventario = prompt("¿Qué tipo de movimieto hará?.\n Bancario = 0 \n Inventario = 1 \n En caso desee terminar, escriba salir.")
+    while (!inventario.trim()){
+        inventario = prompt("¿Qué tipo de movimieto hará?.\n Bancario = 0 \n Inventario = 1 \n En caso desee terminar, escriba salir.")
+        if(!inventario){
+            alert("El campo no puede estar vacío. Inténtelo de nuevo.")
+        }
+    }
     if(inventario !== 0 || inventario !== 1){
         if(inventario == 0){
-            tipoMovimiento = parseInt(prompt("Escriba el número del tipo de movimiento de la lista: \n Egreso = 0\n Ingreso = 1 \n Transferencia =2"))
+            tipoMovimiento = parseInt(prompt("Escriba el número del tipo de movimiento de la lista: \n Egreso = 0\n Ingreso = 1 \n Transferencia = 2"))
             if(tipoMovimiento === 0 || tipoMovimiento === 1 || tipoMovimiento === 2 ){
-                alert(`Ha seleccionado hacer un movimiento ${inventario} tipo ${tipoMovimiento}`)
+                switch(tipoMovimiento){
+                case 0: 
+                    alert(`Ha seleccionado hacer un movimiento Bancario tipo Egreso}`)
+                break
+                case 1: 
+                alert(`Ha seleccionado hacer un movimientoB Bancario tipo Ingreso`)
+                break
+                case 2: 
+                alert(`Ha seleccionado hacer un movimiento Bancario tipo Transferencia`)     
+                break
+                }
             } else{
                 alert("Ingrese un tipo de movimiento válido")
             }   
         }
             if(inventario == 1){
-                tipoMovimiento = parseInt(prompt("Escriba el número del tipo de movimiento de la lista: \n Compra = 0\n Venta = 1 \n Intercambio =2"))
+                tipoMovimiento = parseInt(prompt("Escriba el número del tipo de movimiento de la lista: \n Compra = 0\n Venta = 1 \n Intercambio = 2"))
                 if(tipoMovimiento === 0 || tipoMovimiento === 1 || tipoMovimiento === 2 ){
-                    alert(`Ha seleccionado hacer un movimiento ${inventario} tipo ${tipoMovimiento}`)
+                    switch(tipoMovimiento){
+                        case 0: 
+                            alert(`Ha seleccionado hacer un movimiento de Inventario tipo Compra}`)
+                        break
+                        case 1: 
+                        alert(`Ha seleccionado hacer un movimiento de Inventario tipo Venta`)
+                        break
+                        case 2: 
+                        alert(`Ha seleccionado hacer un movimiento de Inventario tipo Intercambio`)     
+                        break
+                        }
                 } else{
                     alert("Ingrese un tipo de movimiento válido")
                 }
